241a6aa362b3c9e887deecd14324ccf09fdd6bf3
Desa2k <109931246+Desa2k@users.noreply.github.com>
2022-08-06 23:59:31 -0400

    Add files via upload

---
+++ a3_infiSTAR_Exile_customApps/app_defines.hpp
@@ -0,0 +1,8 @@
+class customapp_2 {
+	submenu = 0;
+	toggleable = 0;
+	text = "Unit Scanner";
+	tooltip = "Unit Scanner";
+	fnc = "closeDialog 1; createDialog ""d2kUnitScanner"";";
+	pic = "d2k\UnitScanner\icons\logo.paa";
+};
